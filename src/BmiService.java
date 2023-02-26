b24a70fcff96b3ee700f3455c321f6b7b599db7a
Aleksandr Petrenko <aleksandr_petrenko62@mail.ru>
2023-02-26 18:42:06 +0300

    dorabotka

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate (int mass , int height) {
-        int IBM = mass / height * height;
-        return IBM;
+    public int calculate(int mass, int height) {
+        int imb = mass / height * height;
+        return imb;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         BmiService service = new BmiService();
         int mass = 98;
         int height = (int) 1.98;
-        int IBM = service.calculate(mass , height);
-        System.out.println(IBM);
+        int imb = service.calculate(mass, height);
+        System.out.println(imb);
     }
 }
